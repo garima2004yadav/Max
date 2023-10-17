db7b423d3c9f8f82fe30b07187c1898d05162552
garima2004yadav <115277218+garima2004yadav@users.noreply.github.com>
2023-10-17 22:11:08 +0530

    Add files via upload

---
+++ Max.java
@@ -0,0 +1,12 @@
+public class Max {
+    public static void main(String args[]) {
+        int[] arr = {5,8,1,3,4};
+        int max = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(max < arr[i])
+            max = arr[i];
+        }
+        System.out.println("Max Element is" + max);
+    }
+    
+}
